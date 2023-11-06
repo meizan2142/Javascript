89f2a93f7692f73731f16331f3788227f7277061
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-06 20:20:45 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,12 @@ var accountPassword = "12345"
 accountCity = "Chattogram"
 let accountState;
 
+/*
+prefer not to use var
+because of issue in block space and functional scope
+
+*/
+
 
 accountEmail = "meizan39@gmail.com"
 accountPassword = "2144956"
