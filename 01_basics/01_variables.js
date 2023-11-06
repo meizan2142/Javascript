3b60df6cf40b576279ab8a6ad25f7a952a2c026c
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-06 17:46:59 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accounId = 144553
+let accountEmail = "saifsahai2002@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chattogram"
+let accountState;
+
+
+accountEmail = "meizan39@gmail.com"
+accountPassword = "2144956"
+accountCity = "Dhaka"
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
