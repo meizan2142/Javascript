db38b750c48fbf345cb20f26db372a97d23cd661
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-06 20:19:59 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"Use strict"// treat all js code as newer edition
+// alert(3+3)// we are using nodejs, not browser
+
+let name = "Saif"
+let age = 21
+let isloggedIn = false
+
+// number = 2 to the power 53
+// bigint
+//string = "" 
+// boolean = ture/false
+// null= standalone value
+//undefined=  
+//symbol = unique
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
