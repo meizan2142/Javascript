102035907540183725fac80bda44e21ecf9d4301
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-08 04:54:09 +0000

    Datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 39 
+
+// console.log(score);
+// console.log(typeof score);
+// node 01_basics/03_conversionOperation.js
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN (Not an Number)
+// true =1 ; false= 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true = 1 ; false = 0 
+// "" = false
+//"Saif" = ture
+
+let someNumber = 39
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
