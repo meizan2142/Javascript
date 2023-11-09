3731492eab2dc5b81fd2e466ccbebfe687940949
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-09 17:55:13 +0000

    Datatype concepts

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,17 +1,43 @@
 // console.log("2" < 1);
 // node 01_basics/04_comparison.js
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
 // *********** Datatypes Summary ********** //
 
 // Primitive
 // 7 types are: String, Boolean, Null, Number, Undefined, Symbol, BigInt
 
+const isLoggedIn =  false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("1245")
+const anotherId = Symbol("1231234")
+
+const bigNumber = 324023004n
 
 
 
 // Reference 
 // Array, Objects, Functions
+
+// Arrays
+
+
+
+// objects
+
+const heros = ["Chirstiano", "Neymar", "Messi"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
