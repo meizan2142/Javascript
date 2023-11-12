b9e851dee07e6dd691278a50963e552d54a7a79a
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-12 03:24:34 +0000

    Strings

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -8,4 +8,15 @@ const repoCount = "1"
 // another way to define Strings
 const gameName =  String('SaifSultanMeizan')
 
-console.log(gameName[7]);
+// console.log(gameName[7]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('e'));
+
+const NewString = gameName.substring(0, 2) // we can't use any Negative value here.
+// console.log(NewString);
+// console.log(typeof NewString);
+
+const anotherString = gameName.slice(-16, 2)
+console.log(anotherString);
