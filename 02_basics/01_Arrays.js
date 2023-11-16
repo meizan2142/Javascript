2980acba146eb4e157020c3d13ce66585380e580
Saif Sultan Meizan <149929690+meizan2142@users.noreply.github.com>
2023-11-16 06:30:25 +0000

    Arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,17 @@
+// node 02_basics/01_Arrays.js
+
+//Arrays
+
+const fruits = ["Banana", "Apple", "Pineapple"]
+// console.log(fruits);
+// console.log(typeof fruits);
+// console.log(fruits.join("*"));
+// console.log(fruits.pop());
+// console.log(fruits.push("Saif"));
+
+const marvel_heros = ["IronMan", "Spiderman", "Thor", "CaptainAmerica"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+
